39c1661514b8d9f7ed383d7cd709a9871ea576ee
wexing <edvinas@wex.lt>
2014-12-08 09:33:30 +0200

    Bandymui

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+	
+	
+	class bandymas{
+		public $name;
+		public function __construct(){
+
+			echo "konstruktorius <br>";
+		}
+		public function edvinas($name){
+
+			echo 'labas as vel gryzau ' . $name;
+
+		}
+
+		public function __destruct(){
+
+			echo "<br> destruktorius ";
+		}
+	    
+	}
+	
+	$funk = new bandymas();
+
+	$funk->edvinas('edvinukas')	;
+
+
+
+
+?>
